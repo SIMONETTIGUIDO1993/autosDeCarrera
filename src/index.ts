b4fa2234852479084b60ada0da41b32c1a15bec7
SIMONETTIGUIDO1993 <104177337+SIMONETTIGUIDO1993@users.noreply.github.com>
2022-05-01 03:22:03 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let vuelta1: number = 5;
+let vuelta2: number = 5.5;
+let vuelta3: number = 6;
+let vuelta4: number = 6.5;
+
+let tiempototal: number = vuelta1 + vuelta2 + vuelta3 + vuelta4;
+console.log("Tiempo total de carrera", tiempototal);
+
+let promedioDeVueltas: number = tiempototal / 4;
+console.log("Tiempo promedio por vuelta", promedioDeVueltas);
